2a22276dee168df41e098993c6dc0ff992deae1e
MMy <>
2023-01-12 15:11:35 +0100

    Rotational cipher

---
+++ csharp/Tdd/Tdd/RotationalCipher.cs
@@ -0,0 +1,17 @@
+﻿namespace Tdd
+{
+    public class RotationalCipher
+    {
+        public static string Rotate(string text, int shiftKey)
+            => new(text.Select(c => RotateChar(c, shiftKey)).ToArray());
+
+        private static char RotateChar(char c, int shiftKey)
+        {
+            if (!char.IsLetter(c))
+                return c;            
+                
+            char baseChar = char.IsUpper(c) ? 'A' : 'a';                
+            return (char)((c - baseChar + shiftKey) % 26 + baseChar);
+        }
+    }
+}
---
+++ csharp/Tdd/TddTest/RotationalCipherTest.cs
@@ -0,0 +1,19 @@
+﻿using Tdd;
+
+namespace TddTest
+{
+    public class RotationalCipherTest
+    {
+
+        [Theory]
+        [InlineData("omg", 5, "trl")]
+        [InlineData("o", 5, "t")]
+        [InlineData("Cool", 26, "Cool")]
+        [InlineData("The quick brown fox jumps over the lazy dog.", 13, "Gur dhvpx oebja sbk whzcf bire gur ynml qbt.")]
+        [InlineData("Gur dhvpx oebja sbk whzcf bire gur ynml qbt.", 13, "The quick brown fox jumps over the lazy dog.")]
+        public void Encrypt_returns_expected_ciphertext(string text, int shiftKey, string expected)
+        {
+            Assert.Equal(expected, RotationalCipher.Rotate(text,shiftKey));
+        }
+    }
+}
