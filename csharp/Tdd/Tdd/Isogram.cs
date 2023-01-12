d8ee95d37d41a2e90295c63d62a2db40b4ff33db
MMy <>
2023-01-12 23:16:00 +0100

    Isogram

---
+++ csharp/Tdd/Tdd/Isogram.cs
@@ -0,0 +1,18 @@
+﻿namespace Tdd
+{
+    public class Isogram
+    {
+        public static bool IsIsogram(string input)
+        {
+            HashSet<char> characters = new HashSet<char>();
+
+            foreach (char c in input.ToLower())
+            {
+                if (c == ' ' || c == '-') continue;
+                if (!characters.Add(c))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ csharp/Tdd/TddTest/IsogramTest.cs
@@ -0,0 +1,16 @@
+﻿using Tdd;
+
+namespace TddTest
+{
+    public class IsogramTest
+    {
+        [Theory]
+        [InlineData("isogram", true)]
+        [InlineData("isograms", false)]
+        [InlineData("isogram-bc-de", true)]
+        public void Check_if_input_is_isogram(string input, bool result)
+        {
+            Assert.Equal(result, Isogram.IsIsogram(input));
+        }
+    }
+}
