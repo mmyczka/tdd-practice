8bbbc8dfaf90e2a9c301809e1c52fe8427f17e04
MMy <>
2023-01-12 19:10:00 +0100

    Tom responses

---
+++ csharp/Tdd/Tdd/Tom.cs
@@ -0,0 +1,28 @@
+﻿namespace Tdd
+{
+    public class Tom
+    {
+        public static string Response(string input)
+        {
+            input = input.Trim();
+
+            if (IsYelling(input) && input.EndsWith('?'))
+                return "Calm down, I know what I'm doing!";
+
+            if(IsYelling(input))
+                return "Whoa, chill out!";
+
+            if (input.EndsWith('?'))
+                return "Sure.";
+
+            if (string.IsNullOrWhiteSpace(input))
+                return "Fine. Be that way!";
+
+            return "Whatever.";
+        }
+
+        private static bool IsYelling(string input)
+            => input == input.ToUpper()
+            && input.Any(c => char.IsLetter(c));
+    }
+}
---
+++ csharp/Tdd/TddTest/TomTest.cs
@@ -0,0 +1,19 @@
+﻿using Tdd;
+
+namespace TddTest
+{
+    public  class TomTest
+    {
+        [Theory]
+        [InlineData("How are you?", "Sure.")]
+        [InlineData("I'M FINE", "Whoa, chill out!")]
+        [InlineData("CAN YOU OPEN THE DOOR?", "Calm down, I know what I'm doing!")]
+        [InlineData("", "Fine. Be that way!")]
+        [InlineData("Hello.", "Whatever.")]
+        [InlineData("Okay if like my  spacebar  quite a bit?   ", "Sure.")]
+        public void Test_Tom_responses(string input, string expected)
+        {
+            Assert.Equal(expected, Tom.Response(input));
+        }
+    }
+}
