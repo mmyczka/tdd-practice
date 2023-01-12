430d85828c11839909b4c636183fa5bd2afd8ab5
MMy <>
2023-01-12 21:35:10 +0100

    Pangram

---
+++ csharp/Tdd/Tdd/Pangram.cs
@@ -0,0 +1,9 @@
+﻿namespace Tdd
+{
+    public class Pangram
+    {
+        const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        public static bool IsPangram(string input) 
+            => alphabet.All(x => input.ToLower().Contains(x));
+    }
+}
---
+++ csharp/Tdd/TddTest/PangramTest.cs
@@ -0,0 +1,28 @@
+﻿using Tdd;
+
+namespace TddTest
+{
+    public class PangramTest
+    {
+        [Fact]
+        public void Check_if_text_is_a_pangram()
+        {
+            var input = "The quick brown fox jumps over the lazy dog.";
+            Assert.True(Pangram.IsPangram(input));
+        }
+
+        [Fact]
+        public void Check_if_text_is_a_pangram_too()
+        {
+            var input = "The quick brown fox jumps over The lazy dog.";
+            Assert.True(Pangram.IsPangram(input));
+        }
+
+        [Fact]
+        public void Check_if_text_is_not_a_panagram()
+        {
+            var input = "abc";
+            Assert.False(Pangram.IsPangram(input));
+        }
+    }
+}
