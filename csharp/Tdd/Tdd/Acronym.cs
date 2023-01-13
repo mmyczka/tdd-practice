8673126432d0fa026d77cf2695e70d8e9911657a
MMy <>
2023-01-13 08:56:05 +0100

    Acronym

---
+++ csharp/Tdd/Tdd/Acronym.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace Tdd
+{
+    public class Acronym
+    {
+        public static string Abbreviate(string input)
+        {
+            string[] words = input.Split(new char[] {'-', ' '}, StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder acronym = new StringBuilder();
+
+            foreach (string word in words)
+            {
+                string cleanWord = new(word.Where(c => char.IsLetter(c)).ToArray());
+                acronym.Append(cleanWord[0]);
+            }
+
+            return acronym.ToString().ToUpper();
+        }
+    }
+}
---
+++ csharp/Tdd/TddTest/AcronymTest.cs
@@ -0,0 +1,18 @@
+﻿using Tdd;
+
+namespace TddTest
+{
+    public class AcronymTest
+    {
+        [Theory]
+        [InlineData("As Soon As Possible", "ASAP")]
+        [InlineData("In My Opinion", "IMO")]
+        [InlineData("Liquid-crystal display", "LCD")]
+        [InlineData("Thank Mick It's Today!", "TMIT")]
+        [InlineData("I Like _Ice-Cream", "ILIC")]
+        public void GenerateProperAcronym(string input, string expected)
+        {
+            Assert.Equal(expected, Acronym.Abbreviate(input));
+        }
+    }
+}
