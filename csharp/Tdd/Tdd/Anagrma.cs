fe1cf9fadcc1c189b0a64383165dc471113a6ea4
MMy <>
2023-01-13 20:05:15 +0100

    Anagram

---
+++ csharp/Tdd/Tdd/Anagrma.cs
@@ -0,0 +1,26 @@
+﻿namespace Tdd
+{
+    public class Anagram
+    {
+        private string _word;
+        private string _wordSorted;
+
+        public Anagram(string word)
+        {
+            _word = word.ToLower();
+            _wordSorted = SortedWord(_word);
+        }
+
+        public string[] FindAnagrams(string[] candidates)
+            => candidates.Where(x => IsAnagram(x.ToLower())).ToArray();
+
+        private bool IsAnagram(string candidate)
+            => SortedWord(candidate) == _wordSorted && IsDifferentFromBase(candidate);
+
+        private bool IsDifferentFromBase(string candidate)
+            => candidate != _word;
+
+        private static string SortedWord(string word)
+            => String.Concat(word.OrderBy(c => c)); 
+    }
+}
---
+++ csharp/Tdd/TddTest/AnagramTest.cs
@@ -0,0 +1,28 @@
+﻿using Tdd;
+
+namespace TddTest
+{
+    public class AnagramTest
+    {
+        [Fact]
+        public void Check_if_given_words_are_anagrams_for_stone()
+        {
+            string target = "stone";
+            string[] candidates = { "stone", "tones", "banana", "tons", "notes", "Seton" };
+            string[] expected = { "tones", "notes", "Seton" };
+
+            var anagram = new Anagram(target);
+
+            Assert.Equal(expected, anagram.FindAnagrams(candidates));
+        }
+
+        [Fact]
+        public void Check_if_given_words_are_anagrams_for()
+        {
+            var candidates = new[] { "cashregister", "carthorse", "radishes" };
+            var anagram = new Anagram("Orchestra");
+            var expected = new[] { "carthorse" };
+            Assert.Equal(expected, anagram.FindAnagrams(candidates));
+        }
+    }
+}
