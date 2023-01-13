739c9792d489211cd57e59e10620e946e5d99e1c
MMy <m.myczka@gmail.com>
2023-01-13 23:18:28 +0100

    rle

---
+++ csharp/Tdd/Tdd/Rle.cs
@@ -0,0 +1,47 @@
+﻿namespace Tdd
+{
+    public class Rle
+    {
+        public static string Encode(string text)
+        {
+            string code = "";
+            char current = text[0];
+            int cnt = 0;
+
+            foreach(char c in text)
+            {
+                if (c == current)
+                    cnt++;
+                else
+                {
+                    
+                    code += cnt == 1 ? current : (cnt.ToString() + current);
+                    current = c;
+                    cnt = 1;
+                }
+            }
+            code += cnt == 1 ? current : (cnt.ToString() + current);
+            return code;
+        }
+
+        public static string Decode(string code)
+        {
+            string text = "";
+            int cnt = 0;
+
+            foreach(var c in code)
+            {
+                if (Char.IsNumber(c))
+                    cnt = cnt * 10 + (c - '0');
+                else
+                {
+                    text += new string(c, cnt == 0 ? 1 : cnt);
+                    cnt = 0;
+                }
+
+            }
+
+            return text;
+        }
+    }
+}
---
+++ csharp/Tdd/TddTest/RleTest.cs
@@ -0,0 +1,25 @@
+﻿using Tdd;
+
+namespace TddTest
+{
+    public class RleTest
+    {
+        [Fact]
+        public void Encode_text_using_rle()
+        {
+            var text = "AABCCCDEEEE";
+            var expected = "2AB3CD4E";
+
+            Assert.Equal(expected, Rle.Encode(text));
+        }
+
+        [Fact]
+        public void Decode_text_using_rle()
+        {
+            var text = "2AB3CD4E";
+            var expected = "AABCCCDEEEE";
+
+            Assert.Equal(expected, Rle.Decode(text));
+        }
+    }
+}
