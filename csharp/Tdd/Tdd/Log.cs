1701c76f17cbaca94b1bc37f314029723fa6554b
MMy <m.myczka@gmail.com>
2023-01-14 15:40:12 +0100

    Log analyzer

---
+++ csharp/Tdd/Tdd/Log.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tdd
+{
+    public static class Log
+    {
+        public static string SubstringAfter(this string str, string separator)
+            => str.Split(separator).Last();
+
+        public static string SubstringBetween(this string str, string start, string end)
+            => str.SubstringAfter(start).Split(end).First();
+
+        public static string Message(this string str)
+            => str.SubstringAfter(": ");
+
+        public static string LogLevel(this string str)
+            => str.SubstringBetween("[", "]");
+        
+    }
+}
---
+++ csharp/Tdd/TddTest/LogTest.cs
@@ -0,0 +1,43 @@
+﻿using Tdd;
+
+namespace TddTest
+{
+    public class LogTest
+    {
+        [Fact]
+        public void Return_substring_after_separator()
+        {
+            var log = "[INFO]: File Deleted.";
+            Assert.Equal("File Deleted.", log.SubstringAfter(": ")); 
+        }
+        [Fact]
+        public void Return_substring_after_separator_II()
+        {
+            var log = "[INFO]";
+            Assert.Equal("INFO]", log.SubstringAfter("["));
+        }
+
+        [Fact]
+        public void Return_substring_between_two_strings()
+        {
+            var log = "[INFO]: File Deleted.";
+            Assert.Equal("INFO", log.SubstringBetween("[", "]")); // => returns "INFO"
+        }
+
+        [Fact]
+        public void Return_message()
+        {
+            var log = "[ERROR]: Missing ; on line 20.";
+            Assert.Equal("Missing ; on line 20.", log.Message());
+        }
+
+        [Fact]
+        public void Return_log_level()
+        {
+            var log = "[ERROR]: Missing ; on line 20.";
+            Assert.Equal("ERROR", log.LogLevel());
+        }
+
+
+    }
+}
