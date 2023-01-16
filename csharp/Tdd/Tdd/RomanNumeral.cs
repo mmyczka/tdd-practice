0822135e0eca2583bb91b252aae99dd9982c93c1
MMy <m.myczka@gmail.com>
2023-01-16 14:30:22 +0100

    Roman Numerals

---
+++ csharp/Tdd/Tdd/RomanNumeral.cs
@@ -0,0 +1,29 @@
+﻿namespace Tdd
+{
+    public static class RomanNumeral
+    {
+        public static string ToRoman(this int number)
+        {
+            if (number < 1 || number > 3999) 
+                throw new ArgumentOutOfRangeException("number", "The number must be between 1 and 3999.");
+
+            string result = string.Empty;
+
+            while (number >= 1000) { result += "M"; number -= 1000; }
+            while (number >= 900) { result += "CM"; number -= 900; }
+            while (number >= 500) { result += "D"; number -= 500; }
+            while (number >= 400) { result += "CD"; number -= 400; }
+            while (number >= 100) { result += "C"; number -= 100; }
+            while (number >= 90) { result += "XC"; number -= 90; }
+            while (number >= 50) { result += "L"; number -= 50; }
+            while (number >= 40) { result += "XL"; number -= 40; }
+            while (number >= 10) { result += "X"; number -= 10; }
+            while (number >= 9) { result += "IX"; number -= 9; }
+            while (number >=5 ) { result += "V"; number -= 5; }
+            while (number >= 4) { result += "IV"; number -= 4; }
+            while (number >= 1) { result += "I"; number -= 1; }  
+            
+            return result;
+        }
+    }
+}
---
+++ csharp/Tdd/TddTest/RomanNumeralsTest.cs
@@ -0,0 +1,36 @@
+﻿using Tdd;
+namespace TddTest
+{
+    public class RomanNumeralsTest
+    {
+        [Theory]
+        [InlineData(1, "I")]
+        [InlineData(2, "II")]
+        [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        [InlineData(9, "IX")]
+        [InlineData(10, "X")]
+        [InlineData(40, "XL")]
+        [InlineData(50, "L")]
+        [InlineData(90, "XC")]
+        [InlineData(100, "C")]
+        [InlineData(400, "CD")]
+        [InlineData(500, "D")]
+        [InlineData(900, "CM")]
+        [InlineData(1000, "M")]
+        [InlineData(1985, "MCMLXXXV")]
+
+        public void Int_to_roman_numerals(int number, string expected)
+        {
+            Assert.Equal(expected, number.ToRoman());
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(4000)]
+        public void Int_to_roman_numerals_out_of_range_exception(int number)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => number.ToRoman());
+        }
+    }
+}
