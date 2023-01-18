859189e01aea0c7c70c8e5e95948110088e660fe
MMy <m.myczka@gmail.com>
2023-01-18 10:20:22 +0100

    Phone number

---
+++ csharp/Tdd/Tdd/PhoneNumber.cs
@@ -0,0 +1,14 @@
+﻿namespace Tdd
+{
+    public class PhoneNumber
+    {
+        public static (bool, bool, string) Analyze(string number)
+        {
+            string[] elements = number.Split('-');
+            return (elements[0] == "212", elements[1] == "555", elements[2]);
+        }
+
+        public static bool IsFake((bool isCity, bool isFake, string Lokal) input)
+            => input.isFake;
+    }
+}
---
+++ csharp/Tdd/TddTest/PhoneNumberTest.cs
@@ -0,0 +1,27 @@
+﻿using Tdd;
+
+namespace TddTest
+{
+    public class PhoneNumberTest
+    {
+        [Theory]
+        [InlineData("123-123-1234", false, false, "1234")]
+        [InlineData("212-555-4321", true, true, "4321")]
+        public void Analyze_phone_number(string number, bool IsCity, bool IsFake, string Local)
+        {
+            (bool, bool, string) expected = (IsCity, IsFake, Local);
+            Assert.Equal(expected, PhoneNumber.Analyze(number));
+        }
+
+        [Theory]
+        [InlineData(false, false, "1234", false)]
+        [InlineData(true, true, "4321", true)]
+        public void Analyze_if_phone_number_is_fake(bool IsCity, bool IsFake, string Local, bool expected)
+        {
+            (bool, bool, string) input = (IsCity, IsFake, Local);
+            Assert.Equal(expected, PhoneNumber.IsFake(input));
+        }
+
+    }
+}
+ 
